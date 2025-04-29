be4d442c744090b31ea7ada6f2dccf542dd7c878
sevinc28 <jhasimova28@gmail.com>
2025-04-29 15:50:10 +0000

    task4

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("". join(chr(i) for i in range(97, 123) if chr(i) != 'q' and chr(i) != 'e'), end=" ")
+for i in range(97,123):
+    if i != 101 and i != 113:
+        print("{}". format(chr(i)),
