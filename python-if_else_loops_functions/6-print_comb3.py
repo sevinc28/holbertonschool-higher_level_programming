8a60160655eb35b8cb2ab5214decc44408ab6d16
sevinc28 <jhasimova28@gmail.com>
2025-04-29 16:35:50 +0000

    task3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-print(", ".join("{:02d}{:02d}".format(i, j)
-      for i in range(10)
-      for j in range(i + 1, 10)))
+print(", ".join("{:02d}{:02d}".format(i, j) for i in range(10) for j in range(i + 1, 10)))
