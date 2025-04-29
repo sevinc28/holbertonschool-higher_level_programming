0cbf98449b6fa4f9576b0a56da9608f041c8c191
sevinc28 <jhasimova28@gmail.com>
2025-04-29 16:48:50 +0000

    task4

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-print(", ".join("{:02d}{:02d}".format(i, j)
-      for i in range(10)
-      for j in range(i + 1, 10)))
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}, ".format(i, j), end="")
