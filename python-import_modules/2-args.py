bf44eebdc8d19635527059b0800743d12699eb73
sevinc28 <jhasimova28@gmail.com>
2025-05-01 14:45:21 +0000

    task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 import sys
 
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1  
-    
+    num_args = len(sys.argv) - 1
+
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
