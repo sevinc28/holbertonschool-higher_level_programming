9ecaad0547f2952317c4c86e17f4e864204665c9
sevinc28 <jhasimova28@gmail.com>
2025-05-01 13:34:07 +0000

    task3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     num_args = len(sys.argv) - 1  # Exclude the script name itself from the count
     
     if num_args == 0:
-        print("0 arguments.")
+        print("0 arguments:")
     elif num_args == 1:
         print("1 argument:")
         print(f"1: {sys.argv[1]}")
