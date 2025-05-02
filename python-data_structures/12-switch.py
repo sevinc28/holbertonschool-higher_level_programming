246e6708de2a64d98c5da2e25a9ecf6ae594f6b9
sevinc28 <jhasimova28@gmail.com>
2025-05-02 12:07:44 +0000

    task12

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-a = 10
-b = 89
-# Switch the values of a and b
-a, b = b, a
-print(f"a={a} - b={b}")
+a = 89
+b = 10
+a, b = b, a  # Swap values of a and b
+print("a={:d} - b={:d}".format(a, b))
