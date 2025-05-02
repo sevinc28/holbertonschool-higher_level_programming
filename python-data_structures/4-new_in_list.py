3fda04a61110b38a614bc16343a33c105fd1da58
sevinc28 <jhasimova28@gmail.com>
2025-05-02 10:52:26 +0000

    task4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    # Create a copy of the list to ensure the original list is not modified
     new_list = my_list[:]
+    
+    # Check if the index is valid
     if idx >= 0 and idx < len(my_list):
-        new_list[idx] = element 
-    return new_list
+        new_list[idx] = element  # Replace the element at the given index
+    
+    return new_list  # Return the new list
