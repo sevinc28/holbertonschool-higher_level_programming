effdea83ecd12f9dc989670d0c0c7d1c786e8df6
sevinc28 <jhasimova28@gmail.com>
2025-05-02 11:33:34 +0000

    task10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    # Create an empty list to store the results
     result = []
     
+    # Iterate through each number in my_list
     for num in my_list:
+        # Check if the number is divisible by 2
         result.append(num % 2 == 0)
     
+    # Return the result list
     return result
