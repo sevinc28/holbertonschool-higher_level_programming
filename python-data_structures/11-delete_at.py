f21f5bce846d2fa18359b32ea17d2ab178dda687
sevinc28 <jhasimova28@gmail.com>
2025-05-02 12:08:57 +0000

    task11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    # Create a new list excluding the element at idx
-    return my_list[:idx] + my_list[idx+1:]
+    if idx >= 0 and idx < len(my_list):
+        my_list = my_list[:idx] + my_list[idx+1:]
+    return my_list
