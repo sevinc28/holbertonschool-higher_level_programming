75c9bb7f099c95ef2d46399a7ace26d8a73877cc
sevinc28 <jhasimova28@gmail.com>
2025-05-05 13:51:35 +0000

    task4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,19 +3,30 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
+            # Elemanları alıyoruz
             a = my_list_1[i]
             b = my_list_2[i]
+            
+            # Sayısal olmayan bir tür varsa
             if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
                 print("wrong type")
                 result.append(0)
             else:
+                # Bölme işlemi
                 result.append(a / b)
+        
         except ZeroDivisionError:
+            # Bölme sıfıra yapılırsa
             print("division by 0")
             result.append(0)
+        
         except IndexError:
+            # Liste uzunluğu yetersizse
             print("out of range")
             result.append(0)
+        
         finally:
+            # Her durumda işlemi bitiririz
             continue
+    
     return result
