be2553b277939fd100f6d8567993f7b2d21eb0be
sevinc28 <jhasimova28@gmail.com>
2025-05-05 12:07:28 +0000

    task2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
+    for i in range(x):  # Dövrümüzü x qədər dönmək üçün yazırıq.
         try:
-            if isinstance(my_list[i], int):  # Check if the element is an integer
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
+            if isinstance(my_list[i], int):  # Elementin tam ədəd olub olmadığını yoxlayırıq
+                print("{:d}".format(my_list[i]), end="")  # Tam ədəd isə onu çap edirik.
+                count += 1  # Çap edilmiş tam ədədin sayını artırırıq.
         except IndexError:
-            break
-    print()  # Print a new line after the printed integers
-    return count
+            break  # Əgər x, my_list uzunluğundan böyükdürsə, dövrü dayandırırıq.
+    print()  # Növbəti sətir üçün bir boşluq əlavə edirik.
+    return count  # Çap edilmiş tam ədədlərin sayını qaytarırıq.
