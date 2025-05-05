d8c0ea784d7e25e623a84bdd0db915a23d1743ee
sevinc28 <jhasimova28@gmail.com>
2025-05-05 13:59:53 +0000

    task4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,11 +3,11 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            # Elemanları alıyoruz
+            # Elemanları alıyor
             a = my_list_1[i]
             b = my_list_2[i]
             
-            # Sayısal olmayan bir tür varsa
+            # Sayısal olmayan bir tür var
             if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
                 print("wrong type")
                 result.append(0)
