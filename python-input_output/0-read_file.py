7f1d386129760a59657f85bb46c8c665d75a0ed5
sevinc28 <jhasimova28@gmail.com>
2025-06-04 07:14:47 +0000

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+#bu modul (UTS-8 formatinda) oxuyan ve mezmunu cap eden funksiyani ehate edir
+"""
 def read_file(filename=""):
-    with open(filename, "r", encoding = "UTF8"
-            print(f.read())
+    """
+    Funksiya UTS-8 formatinda oxuyur
+    """
+    Args:
+        filename(str):faylin adidir
+    """
+     with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
 
