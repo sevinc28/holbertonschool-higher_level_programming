af83d2701ea0a52900009f5c8d9f6c0ac86f35ca
sevinc28 <jhasimova28@gmail.com>
2025-06-04 07:18:29 +0000

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-#bu modul (UTS-8 formatinda) oxuyan
-ve mezmunu cap eden funksiyani ehate edir.
+This module contains a function that reads a text file (UTF8)
+and prints its content to stdout.
 """
 
 
 def read_file(filename=""):
     """
-    Funksiya UTS-8 formatinda oxuyur
+    Reads a UTF-8 text file and prints its content to stdout.
 
     Args:
-        filename(str):faylin adidir
+        filename (str): The name of the file to be read.
     """
-     with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
 
