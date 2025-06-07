1a5324d464ba66123d9ed2f8e87d82b6dcbf5de0
sevinc28 <jhasimova28@gmail.com>
2025-06-07 13:43:09 +0000

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,19 +3,19 @@
 
 import sys
 from os.path import exists
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-#Yoxla: fayl movcuddursa< movcud siyahini oxu, eks halda bos siyahi ile basla
+# Yoxla: fayl mövcuddursa, mövcud siyahını oxu, əks halda boş siyahı ilə başla
 if exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
 
-#Elave olunan arqumentleri siyahiya elave et
+# Əlavə olunan arqumentləri siyahıya əlavə et
 items.extend(sys.argv[1:])
 
-#Yenilenmis siyahini fayla yaz
+# Yenilənmiş siyahını fayla yaz
 save_to_json_file(items, filename)
