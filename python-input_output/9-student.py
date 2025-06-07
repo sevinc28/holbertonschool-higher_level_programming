df3705d9dd858c82653772347b1f570e7241c4d3
sevinc28 <jhasimova28@gmail.com>
2025-06-07 13:52:33 +0000

    task9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,7 +5,7 @@
 class Student:
     """Class that defines a student."""
 
-    def __init__(self, first_name< last_name, age):
+    def __init__(self, first_name, last_name, age):
         """Initialize a new Student instance."""
         self.first_name = first_name
         self.last_name = last_name
