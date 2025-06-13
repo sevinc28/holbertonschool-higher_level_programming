cb63197875d1293e17cd9c706707e8bde1228da7
sevinc28 <jhasimova28@gmail.com>
2025-06-13 11:06:32 +0000

    task0

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
-"""Defines a class Student with optional attribute filtering for JSON serialization."""
+"""
+This module defines a Student class that can be serialized to a dictionary.
+"""
 
 
 class Student:
-    """Class that defines a student."""
+    """Defines a student with first_name, last_name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student instance."""
+        """Initializes a new Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation of the Student instance.
-        If attrs is a list of strings, return only those attributes included.
-        Otherwise, return all attributes.
         """
-        if type(attrs) == list and all(type(attr) == str for attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        Returns a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes with names in that list
+        will be included. Otherwise, all attributes are included.
+        """
+        if (isinstance(attrs, list) and
+                all(isinstance(attr, str) for attr in attrs)):
+            result = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    result[key] = getattr(self, key)
+            return result
         return self.__dict__
